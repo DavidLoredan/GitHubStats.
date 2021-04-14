c8ab6ba9c0a96ac571849b7fa4aceb5aa1b6b234
Loredan <davidloredanvicentiu@gmail.com>
2021-04-14 19:45:58 +0200

    header has been modified with title  and logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,24 @@
+import './scss/Header.scss';
+
 function Header() {
-  return <div>Ici on code le Header</div>;
+  return (
+    <div className="loHeader">
+      <div>
+        <img
+          className="loLogo"
+          src="https://i.ibb.co/rfPqm9t/Logo-Makr-3-Rg99-U.png"
+          alt="logoGitStats"
+        />
+      </div>
+      <section className="loBlocColor">
+        <div className="loTitle">
+          <h1>GitHub Stats</h1>
+        </div>
+        <div className="loSousTitre">
+          <h2>Developper Space</h2>
+        </div>
+      </section>
+    </div>
+  );
 }
 export default Header;
