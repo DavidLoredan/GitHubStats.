48b8b90d320540558751ca5ed06daf1ba15a5b64
christopheluc62 <christopheluc62@gmail.com>
2021-04-23 14:56:26 +0200

    update UserStats.jsx

--- src/components/UserStats.jsx
+++ src/components/UserStats.jsx
@@ -1,15 +1,16 @@
+import React from 'react';
 import '../reset.css';
-import './UserStats.scss';
+import './scss/UserStats.scss';
 import UserInfo from './UserInfo';
 import UserStarsRepo from './UserStarsRepo';
-import UserLastRepo from './UserLastRepo';
+import UserRepo from './UserRepo';
 
 function UserStats() {
   return (
     <div className="UserStats">
       <UserInfo />
       <UserStarsRepo />
-      <UserLastRepo />
+      <UserRepo />
     </div>
   );
 }
