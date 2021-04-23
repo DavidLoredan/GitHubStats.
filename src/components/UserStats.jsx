396d45e597643f1c8838e8377d86d22ceb9b9367
erriep <tabary.pierre.dev@gmail.com>
2021-04-23 15:23:05 +0200

    resolve api and add usehistory and useParams v02

--- src/components/UserStats.jsx
+++ src/components/UserStats.jsx
@@ -1,15 +1,13 @@
 import '../reset.css';
-import './UserStats.scss';
+import './scss/UserStats.scss';
 import UserInfo from './UserInfo';
 import UserStarsRepo from './UserStarsRepo';
-import UserLastRepo from './UserLastRepo';
 
 function UserStats() {
   return (
     <div className="UserStats">
       <UserInfo />
       <UserStarsRepo />
-      <UserLastRepo />
     </div>
   );
 }
