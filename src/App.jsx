478ef9fc0f42a148e0e0ec2d5f452e497550d0c1
Hacene010 <hamrounehacene06@gmail.com>
2021-04-28 13:16:58 +0200

    changed from scss to styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import './reset.css';
-import './app.scss';
-import UserStats from './components/UserStats';
-
+import StyledApp from './StyledApp';
 import UserStats from './components/UserStats';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <StyledApp>
         <Header />
 
         <Switch>
@@ -24,7 +21,7 @@ function App() {
         </Switch>
 
         <Footer />
-      </div>
+      </StyledApp>
     </Router>
   );
 }
