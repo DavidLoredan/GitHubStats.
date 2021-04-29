bcd47f4fbd5aae588d71d3a6f5f7512851ba7490
Hacene010 <hamrounehacene06@gmail.com>
2021-04-29 20:26:39 +0200

    uptaded the style of the events

--- src/components/styledComponents/Sevents.jsx
+++ src/components/styledComponents/Sevents.jsx
@@ -1,59 +1,55 @@
 import styled from 'styled-components';
 
 const StyledEvent = styled.div`
-  .InfoContainer {
-    .Infos {
-      justify-content: space-around;
-      display: flex;
+  .Infos {
+    display: flex;
 
-      a {
-        text-decoration: none;
-        color: black;
-      }
+    a {
+      text-decoration: none;
+      color: black;
     }
+  }
 
-    img {
-      position: relative;
-      width: 40%;
-      box-shadow: 5px 5px 5px rgb(95, 92, 92);
-      margin: 15px;
-      border-radius: 45px 10px;
-    }
+  img {
+    margin: 10px;
+    height: 50%;
+  }
 
-    .InfoTitle {
-      padding: 30px;
-      text-align: center;
-      font-size: 2.6em;
-      font-weight: bolder;
-      height: 10vh;
-      background-color: rgb(199, 199, 199);
-      color: grey;
-    }
-    @media (max-width: 700px) {
-      .eventsTitle {
-        font-size: small;
-        .Infos {
-          flex-wrap: nowrap;
-        }
-      }
+  .InfoTitle {
+    padding: 30px;
+    text-align: center;
+    font-size: 2.6em;
+    font-weight: bolder;
+    height: 12vh;
+    background-color: rgb(199, 199, 199);
+  }
+  @media (max-width: 700px) {
+    .eventsTitle {
+      font-size: small;
     }
   }
 
   .SubjectInfo {
     position: relative;
     bottom: 40px;
-    width: 40%;
-    box-shadow: 5px 5px 5px rgb(95, 92, 92);
-    margin: 15px;
-    border-radius: 45px 10px;
-    border: 2px solid black;
-    background-color: rgba(35, 37, 41, 0.5);
+    height: 43vh;
+    width: 35vw;
+    margin-left: 9vw;
+    border: 1px solid black;
+    background-color: rgb(199, 199, 199);
     text-align: center;
   }
 
   .SubjectTitle {
     font-size: 3vh;
     font-weight: bold;
+    margin: 5px;
+  }
+  p {
+    margin: 10px;
+  }
+  .SubjectDate {
+    margin: 10px;
   }
 `;
 export default StyledEvent;
