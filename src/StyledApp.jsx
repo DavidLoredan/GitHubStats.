3177f7a376c3a458d607ceeb145e26dce0b62543
Hacene010 <hamrounehacene06@gmail.com>
2021-05-05 12:09:48 +0200

    removed the styledApp.jsx we do not use

--- src/StyledApp.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const StyledApp = styled.div`
-  background-image: url('https://images.pexels.com/photos/34600/pexels-photo.jpg');
-  background-size: 100vw;
-`;
-
-export default StyledApp;
